1f39617763e5b21de42d3d88f8dac5943dbdabd1
Vladislav Moiseev <moiseev.va15@gmail.com>
2025-01-31 00:42:42 +0500

    Добавлена команда для создания БД create_db.py

---
+++ users/management/commands/create_db.py
@@ -0,0 +1,36 @@
+import psycopg2
+
+from django.core.management import BaseCommand
+
+from config.settings import DATABASE, USER, PASSWORD, HOST, PAD_DATABASE, PORT
+
+
+class Command(BaseCommand):
+    """Создание БД"""
+
+    def handle(self, *args, **options):
+        # ConnectionString = f'''PORT={PORT};
+        #                        SERVER={HOST};
+        #                        DATABASE={PAD_DATABASE};
+        #                        UID={USER};
+        #                        PWD={PASSWORD}'''
+        conn_params = {
+            'dbname': PAD_DATABASE,
+            'user': USER,
+            'password': PASSWORD,
+            'host': HOST,
+            'port': PORT
+        }
+        try:
+            conn = psycopg2.connect(**conn_params)
+        except psycopg2.ProgrammingError as ex:
+            print(ex)
+        else:
+            conn.autocommit = True
+            try:
+                cursor = conn.cursor()
+                cursor.execute(fr'CREATE DATABASE {DATABASE};')
+            except psycopg2.ProgrammingError as ex:
+                print(ex)
+            else:
+                print(f'База данных {DATABASE} успешно создана!')
