e2177c9b82271d0709721fbb38e73dacd3d22208
Vladislav Moiseev <moiseev.va15@gmail.com>
2025-03-13 21:20:21 +0500

    Регистрация класса User в админке джанго

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+@admin.register(models.User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'last_name', 'first_name', 'role', 'pk', 'is_active')
+    list_filter = ('last_name',)
